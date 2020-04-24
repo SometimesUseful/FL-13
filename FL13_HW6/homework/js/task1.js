4e69d8b3268432c4bab15fec5ccea048ef1fa40b
SometimesUseful <smilelxrd@gmail.com>
2020-04-25 01:35:02 +0300

    Comic

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -9,13 +9,18 @@ if(typeof check !== 'number'|| typeof percentage !=='number'){
 } else if (percentage<0 || percentage>100){
         alert('Invalid input data');
     } else {
-         tip = check * percentage / 100;
-         sum = +check + +tip;
+    tip = check * percentage / 100;
+    sum = +check + +tip;
+}
+         if (sum<0){
+             alert('Total sum can`t be negative');
+         } else{
          alert(`Check number:${check}
 Tip:${percentage}%
 Tip amount:${tip}
-Total sum to pay ${sum}`)
-}
+Total sum to pay ${sum}`);
+         }
+
 
 
 
