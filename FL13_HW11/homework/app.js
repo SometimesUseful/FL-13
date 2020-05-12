4fa28b88c16e670503cfa0b9584d02c8e4002736
SometimesUseful <smilelxrd@gmail.com>
2020-05-12 23:46:27 +0300

    Comic

---
+++ FL13_HW11/homework/app.js
@@ -0,0 +1,68 @@
+const data = [
+  {
+    'folder': true,
+    'title': 'Pictures',
+    'children': [
+      {
+        'title': 'logo.png'
+      },
+      {
+        'folder': true,
+        'title': 'Vacations',
+        'children': [
+          {
+            'title': 'spain.jpeg'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Desktop',
+    'children': [
+      {
+        'folder': true,
+        'title': 'screenshots',
+        'children': null
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Downloads',
+    'children': [
+      {
+        'folder': true,
+        'title': 'JS',
+        'children': null
+      },
+      {
+        'title': 'nvm-setup.exe'
+      },
+      {
+        'title': 'node.exe'
+      }
+    ]
+  },
+  {
+    'title': 'credentials.txt'
+  }
+];
+
+const rootNode = document.getElementById('root');
+
+// TODO: your code goes here
+
+let ul = document.createElement('ul');
+let li = document.createElement('li');
+let expand = document.createElement('div');
+let content = document.createElement('div');
+ul.className = 'Container';
+rootNode.appendChild(ul);
+ul.appendChild(li);
+expand.className = 'Expand';
+content.className = 'Content';
+li.appendChild(expand);
+li.appendChild(content);
+li.appendChild(ul);
